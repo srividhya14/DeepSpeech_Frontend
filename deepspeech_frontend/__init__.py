fd2bcc4072b5ac96d2d436518cb43c4d02e0cb61
srividhya14 <65883360+srividhya14@users.noreply.github.com>
2022-02-26 10:53:55 +0530

    Update __init__.py

--- deepspeech_frontend/__init__.py
+++ deepspeech_frontend/__init__.py
@@ -18,6 +18,69 @@ api_keyfile = 'api_keys.txt'
 transcription_in_progress = False
 print(transcription_in_progress)
 
+@app.route('/results/<filename>')
+def transcribe(filename):
+    global transcription_in_progress
+    if(transcription_in_progress):
+        print("Oh no! Another transcription was in progress, waiting 5 seconds...")
+        time.sleep(5)
+        transcribe(filename)
+    print("Starting transcription...")
+    transcription_in_progress = True
+    fs, audio = wav.read(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    processed_data = ds.stt(audio)
+    #os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    transcription_in_progress = False
+    print('Transcript:', processed_data)
+    return processed_data
+
+# Use ffmpeg to convert our file to WAV @ 16k
+def normalize_file(file):
+    filename = str(uuid.uuid4()) + ".wav"
+    fileLocation = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    stream = ffmpeg.input(file)
+    stream = ffmpeg.output(stream, fileLocation, acodec='pcm_s16le', ac=1, ar='16k')
+    ffmpeg.run(stream)
+    return filename
+
+@app.route('/api/v1/process', methods=['POST'])
+def api_transcribe():
+    # check and see if API keys are set
+    if(len(api_keys) > 0):
+        # get the request headers and check the keys
+        if "Authorization" in request.headers:
+            key = request.headers["Authorization"]
+            if(len(key) > 7):
+                if key[7:] not in api_keys:
+                    return make_response(jsonify({'error': 'Your API key is invalid :c'}), 400)
+            else:
+                return make_response(jsonify({'error': 'Your API key was empty :c'}), 400)
+        else:
+            return make_response(jsonify({'error': 'We need an API key to process your request :c'}), 400)
+
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        return make_response(jsonify({'error': 'No file part'}), 400)
+    file = request.files['file']
+    # if the request has a blank filename
+    if file.filename == '':
+        return make_response(jsonify({'error': 'No file in file parameter'}), 400)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        fileLocation = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(fileLocation)
+        convertedFile = normalize_file(fileLocation)
+        # stream = ffmpeg.overwrite_output(stream)
+        os.remove(fileLocation)
+
+        return jsonify({'message' : transcribe(filename=convertedFile)})
+    return make_response(jsonify({'error': 'Something went wrong :c'}), 400)
+
+# Return a JSON error rather than a HTTP 404
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 if os.path.isfile("models/deepspeech-0.9.3-models.pbmm"):
     print("Starting the DeepSpeech Frontend")
     ds = Model('models/deepspeech-0.9.3-models.pbmm')
@@ -27,6 +90,7 @@ elif os.path.isfile("/var/lib/deepspeech/models/deepspeech-0.9.3-models.pbmm"):
     ds.enableExternalScorer('/var/lib/deepspeech/models/deepspeech-0.9.3-models.scorer')
 else:
     sys.exit('No DeepSpeech Model found, please download one!')
+transcribe('audio_final.wav')
 
 
 def load_keys(keylist):
@@ -76,65 +140,3 @@ def upload_file():
     <p>No files or text is retained after a file is processed, we use Mozilla's DeepSpeech library with their default models and settings,
     To contribute, come <a href="https://git.callpipe.com/fusionpbx/deepspeech_frontend/">check out our code</a>! We're also looking for suggestions as how to improve accuracy.</p></center>
     '''
-
-@app.route('/results/<filename>')
-def transcribe(filename):
-    global transcription_in_progress
-    if(transcription_in_progress):
-        print("Oh no! Another transcription was in progress, waiting 5 seconds...")
-        time.sleep(5)
-        transcribe(filename)
-    print("Starting transcription...")
-    transcription_in_progress = True
-    fs, audio = wav.read(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    processed_data = ds.stt(audio)
-    os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    transcription_in_progress = False
-    return processed_data
-
-# Use ffmpeg to convert our file to WAV @ 16k
-def normalize_file(file):
-    filename = str(uuid.uuid4()) + ".wav"
-    fileLocation = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    stream = ffmpeg.input(file)
-    stream = ffmpeg.output(stream, fileLocation, acodec='pcm_s16le', ac=1, ar='16k')
-    ffmpeg.run(stream)
-    return filename
-
-@app.route('/api/v1/process', methods=['POST'])
-def api_transcribe():
-    # check and see if API keys are set
-    if(len(api_keys) > 0):
-        # get the request headers and check the keys
-        if "Authorization" in request.headers:
-            key = request.headers["Authorization"]
-            if(len(key) > 7):
-                if key[7:] not in api_keys:
-                    return make_response(jsonify({'error': 'Your API key is invalid :c'}), 400)
-            else:
-                return make_response(jsonify({'error': 'Your API key was empty :c'}), 400)
-        else:
-            return make_response(jsonify({'error': 'We need an API key to process your request :c'}), 400)
-
-    # check if the post request has the file part
-    if 'file' not in request.files:
-        return make_response(jsonify({'error': 'No file part'}), 400)
-    file = request.files['file']
-    # if the request has a blank filename
-    if file.filename == '':
-        return make_response(jsonify({'error': 'No file in file parameter'}), 400)
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        fileLocation = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(fileLocation)
-        convertedFile = normalize_file(fileLocation)
-        # stream = ffmpeg.overwrite_output(stream)
-        os.remove(fileLocation)
-
-        return jsonify({'message' : transcribe(filename=convertedFile)})
-    return make_response(jsonify({'error': 'Something went wrong :c'}), 400)
-
-# Return a JSON error rather than a HTTP 404
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
